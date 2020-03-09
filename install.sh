0e8a58bab7913e969b6d5e9b25850e5a82a5f4c9
Genevieve L'Esperance <glesperance@pivotal.io>
2020-03-09 10:16:30 -0400

    Ruby homebrew installer is deprecated, use bash.

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ function main() {
 	fi
 
 	if ! [ -x "$(command -v brew)" ]; then
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
 
 	brew update
